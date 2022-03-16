69bd0e6cbffa4e10868bc4a41616493e07365f72
Tammy Fina <finatammy@gmail.com>
2022-03-16 15:57:02 -0400

    fiexed error with module files

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,55 +1,52 @@
-import React from 'react';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-    const  categories = [
-        { name: 'commercial', 
-        description: 
-        'Photos of grocery stores, food trucks, and other commercial projects' 
-        },
-        { name: 'portraits', description: 'Portraits of people in my life' },
-        { name: 'food', description: 'Delicious delicacies' },
-        { 
-            name: 'landscape', 
-            description: 'Fields, farmhouses, waterfalls, and the beauty of nature' 
-        },
-    ];
-    
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
 
-    return (
-        <header>
-            <h2>
-                <a href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-                </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
-                            About me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                    {categories.map((category) => (
-                        // the outermost element must have a key attribute that is set to something unique.  This helps React keep track of items in the virtual DOM
-                        <li
-                            className="mx-1"
-                            key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
-                            </span>
-                        </li>
-                    ))}    
-                </ul>
-            </nav>
-        </header>
-    );
+  const  categories = [
+    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+  ];
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => handleClick("About")}>
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
+          </li>
+          {
+            categories.map((category) => (
+              <li className="mx-1" key={category.name} >
+                <span onClick={() => { handleClick(category.name); }}>
+                 {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
